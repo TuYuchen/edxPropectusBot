4a112ed068ed3219618beeca045eaf046c4df6dc
zxcs001 <zxcs001@gmail.com>
2023-02-08 04:03:23 -0300

    comment change.

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ module.exports = (app) => {
 
 async function dismissPullRequest (context) {
   // Dismiss the PR
+  context.log(context);
   const prParams = context.pullRequest({ reviewers: ['influscopeTu']})
   await context.octokit.pulls.requestReviewers(prParams)
 }
