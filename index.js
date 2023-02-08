d3eeb5ee3c93fd5ee17bf9c18254ee31038a8ce6
zxcs001 <zxcs001@gmail.com>
2023-02-07 23:04:29 -0300

    initial commit.

--- index.js
+++ index.js
@@ -13,6 +13,31 @@ module.exports = (app) => {
     return context.octokit.issues.createComment(issueComment);
   });
 
+  app.on(["push"], async (context) => {
+    context.log("issue_comment.created or issue_comment.edited");
+
+    if (context.isBot) {
+      // Ignore update if this issue was created by the bot
+      context.log("This push was created by the bot");
+      context.log("Execution finished\n\n");
+      return;
+    }
+
+    context.octokit.reactions.createForIssueComment({
+      owner: context.payload.repository.owner.login,
+      repo: context.payload.repository.name,
+      comment_id: context.payload.comment.id,
+      content: "-1"
+    });
+    context.log("Reacted with -1");
+    const issueComment = context.issue({
+      body: "You are not allowed to self-approve others Pull Request!",
+    });
+    await context.octokit.issues.createComment(issueComment);
+
+
+  });
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
