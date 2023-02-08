eaba215e9b35b89ce19845826030ac3d61ad7db0
zxcs001 <zxcs001@gmail.com>
2023-02-08 00:09:41 -0300

    comment change.

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ module.exports = (app) => {
   });
 
   app.on(["pull_request.opened", "pull_request.edited", "pull_request.synchronize"], async (context) => {
-    context.log("issue_comment.created or issue_comment.edited");
+    context.log("The PR was updated recently.");
 
+    // const prUser = context.payload.issue.user.login;
+    context.log("The payload", context.payload);
     if (context.isBot) {
       // Ignore update if this issue was created by the bot
       context.log("This push was created by the bot");
@@ -23,12 +25,6 @@ module.exports = (app) => {
       return;
     }
 
-    // context.octokit.reactions.createForIssueComment({
-    //   owner: context.payload.repository.owner.login,
-    //   repo: context.payload.repository.name,
-    //   comment_id: context.payload.comment.id,
-    //   content: "-1"
-    // });
     context.log("Reacted with -1");
     const issueComment = context.issue({
       body: "Your PR was dismissed due to recent update/s.",
@@ -37,9 +33,11 @@ module.exports = (app) => {
 
   });
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
+
+
+async function dismissPullRequest (context) {
+  // Dismiss the PR
+  const prParams = context.pullRequest({ event: 'DISMISS' })
+  await context.octokit.pulls.createReview(prParams)
+}
